75786f86dc77418d84ccd977044373ba85eda6e5
Unknown <welzwo@gmail.com>
2018-02-03 18:14:25 +0100

    Change scecurity level to 1 in reference JS code

--- tests/iota-js-address-test/index.js
+++ tests/iota-js-address-test/index.js
@@ -5,7 +5,7 @@ var iota = new IOTA({
 const seed = "PETERPETERPETERPETERPETERPETERPETERPETERPETERPETERPETERPETERPETERPETERPETERPETERR";
 iota.api.getNewAddress(seed, {
   index: 0,
-  security: 2,
+  security: 1,
   total: 5
 }, (e, r) => {
   console.log('getNewAddress', e, r);
