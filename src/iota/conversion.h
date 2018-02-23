9c2d7d6365d979412a3a43cd18999a5eabb3813e
Wolfgang Welz <welzwo@gmail.com>
2018-02-23 08:04:22 +0100

    Make chars_to_trits public

--- src/iota/conversion.h
+++ src/iota/conversion.h
@@ -9,6 +9,14 @@
 #include <stdint.h>
 #include "iota_types.h"
 
+/** @brief Converts a balanced ternary number in base-27 encoding into its
+ *         trit representation.
+ *  @param chars base-27 encoded ternary number
+ *  @param trits target trit array
+ *  @param chars_len length of the input char array
+ */
+void chars_to_trits(const char *chars, trit_t *trits, unsigned int chars_len);
+
 /** @brief Converts a single signed integer into its ternary representation.
  *  @param value signed integer to convert
  *  @param trits target trit array
@@ -16,7 +24,7 @@
  *  @return true, if an overflow occured and the given integer could not be
             completely represented with this number of trits, false otherwise.
  */
-bool int64_to_trits(int64_t value, trit_t *trits, size_t num_trits);
+bool int64_to_trits(int64_t value, trit_t *trits, unsigned int num_trits);
 
 /** @brief Converts a balanced ternary number in base-27 encoding into a
  *         big-enadian binary integer.
@@ -26,7 +34,7 @@ bool int64_to_trits(int64_t value, trit_t *trits, size_t num_trits);
  *  @param bytes target byte array
  *  @param chars_len length of the input
  */
-void chars_to_bytes(const char *chars, unsigned char *bytes, size_t chars_len);
+void chars_to_bytes(const char *chars, unsigned char *bytes, unsigned int chars_len);
 
 /** @brief Converts a big-enadian binary integer into a balanced ternary number
  *         in base-27 encoding.
@@ -36,7 +44,7 @@ void chars_to_bytes(const char *chars, unsigned char *bytes, size_t chars_len);
  *  @param chars zero-terminated base-27 encoded ternary representation
  *  @param bytes_len number of input bytes
  */
-void bytes_to_chars(const unsigned char *bytes, char *chars, size_t bytes_len);
+void bytes_to_chars(const unsigned char *bytes, char *chars, unsigned int bytes_len);
 
 /** @brief Sets the 243th trit to zero.
  *  If the byte array represents a balanced ternary number which has the
