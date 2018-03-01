04bd83e6c054039ebe59573110d8bafe4393e3d0
Wolfgang Welz <welzwo@gmail.com>
2018-03-01 12:54:34 +0100

    Add max IOTA value as a constant

--- src/iota/iota_types.h
+++ src/iota/iota_types.h
@@ -7,9 +7,10 @@
 
 typedef int8_t trit_t;
 typedef int8_t tryte_t;
-typedef int8_t trint_t;
 
 #define MIN_TRYTE_VALUE -13
 #define MAX_TRYTE_VALUE 13
+
+#define MAX_IOTA_VALUE INT64_C(2779530283277761) // (3^33-1) / 2
 
 #endif // IOTA_TYPES_H
