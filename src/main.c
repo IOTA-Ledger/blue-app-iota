196319d1b249eef6880070f5f8bef9c2cf3f7629
Pasquale Ambrosini <pasquale.ambrosini@gmail.com>
2018-03-08 19:07:55 +0100

    Better support for address generation

--- src/main.c
+++ src/main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 #include <stdio.h>
-#include <time.h>
+#include <stdlib.h>
 // iota-related stuff
 #include "iota/kerl.h"
 #include "iota/conversion.h"
@@ -12,20 +12,33 @@ void address(char* seedChars, int index, int security, char* result);
 
 int main(int argc, char *argv[]){
 
-	if (argc != 2) {
+	if (argc != 5) {
 		return print_help();
 	}
 
+	int num;
+	int security = (int)strtol(argv[2], NULL, 10);
+	int index = (int)strtol(argv[3], NULL, 10);
+	int count = (int)strtol(argv[4], NULL, 10);
 	char* seed_chars = argv[1];
-	char char_address[81];
-	address(seed_chars, 0, 2, char_address);
-	printf("%s\n", char_address);
+
+	char addresses[(count*82)];
+	addresses[0] = '\0';
+	for (int i = 0; i < count; i++) {
+		char char_address[81];
+		address(seed_chars, index+i, security, char_address);
+		strcat(addresses, char_address);
+		strcat(addresses, "\n");
+	}
+
+
+	printf("%s", addresses);
 	
 	return 0;
 }
 
 int print_help() {
-	printf("Usage c_light_wallet <SEED_81_CHARS>\n");
+	printf("Usage c_light_wallet <SEED_81_CHARS> SECURITY INDEX COUNT\n");
 	return 0;
 }
 
