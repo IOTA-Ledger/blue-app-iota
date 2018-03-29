6c132bbb7794c82f4d1eab032f52f3c66fc0c4e2
Peter Willemsen <peter@codebuffet.co>
2018-03-29 12:37:28 +0200

    removed kerlTest and added bundleTest in JS

--- tests/iota-js-address-test/index.js
+++ tests/iota-js-address-test/index.js
@@ -2,12 +2,18 @@ const IOTA = require('iota.lib.js')
 var iota = new IOTA({
 
 })
-const seed = "PETERPETERPETERPETERPETERPETERPETERPETERPETERPETERPETERPETERPETERPETERPETERPETERR";
-iota.api.getNewAddress(seed, {
-  index: 0,
-  security: 1,
-  total: 5
-}, (e, r) => {
-  console.log('getNewAddress', e, r);
-})
-iota.api.kerlTest();
+
+function bundleTest(signatureSize) {
+  var bundle = new iota.utils.Bundle();
+
+  bundle.addEntry(signatureSize, "LHWIEGUADQXNMRKQSBDJOAFMBIFKHHZXYEFOU9WFRMBGODSNJAPGFHOUOSGDICSFVA9KOUPPCMLAHPHAW", 10, "999999999999999999999999999", 0, 0);
+  bundle.addEntry(signatureSize, "WLRSPFNMBJRWS9DFXCGIROJCZCPJQG9PMOO9CUZNQXTLLQAYXGXT9LECGEQ9MQIWIBGQREFHULPOETHNZ", -5, "999999999999999999999999999", 0, 0);
+  bundle.addEntry(signatureSize, "UMDTJXHIFVYVCHXKZNMQWMDHNLVQNMJMRULXUFRLNFVVUMKYZOAETVQOWSDUAKTXVNDSVAJCASTRQNV9D", -5, "999999999999999999999999999", 0, 0);
+  bundle.finalize();
+
+  console.log('bundle test', bundle);
+}
+
+
+bundleTest(1);
+bundleTest(2);
