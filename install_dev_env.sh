e03d93aacf9c9e5be8f8b87f4b59467f28186d55
muXxer <mux3r@web.de>
2018-03-31 19:16:38 +0200

    Using the CLANG, LLVM and ARM-GCC from LedgerHQ install instructions instead of the Ubuntu packages

--- install_dev_env.sh
+++ install_dev_env.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+## Functions
 _bold=$(tput bold)
 _reset=$(tput sgr0)
 
@@ -11,53 +12,93 @@ function print_ok   { printf "${_bold}${_green}%s${_reset}\n" "$@"; }
 function print_err  { printf "${_bold}${_red}%s${_reset}\n" "$@"; }
 function print_warn { printf "${_bold}${_yellow}%s${_reset}\n" "$@"; }
 
+## Linux Distribution and Version Check
 print_ok "Installing development environment for blue-app-iota..."
 
-if [ `gawk -F= '/^NAME/{print $2}' /etc/os-release` != '"Ubuntu"' ];
+linux_distr=`gawk -F= '/^NAME/{print $2}' /etc/os-release`
+linux_codename=`gawk -F= '/^VERSION_CODENAME/{print $2}' /etc/os-release`
+if [ ${linux_distr} != '"Ubuntu"' ];
 then
     print_err "ERROR: This script was created for Ubuntu only! Exiting..."
     exit 1
 fi
 
-if [ `gawk -F= '/^VERSION_CODENAME/{print $2}' /etc/os-release` != 'artful' ];
+if [ ${linux_codename} != 'artful' ];
 then
     print_err "ERROR: This script was tested under Ubuntu Artful Aardvark only! Exiting..."
     exit 1
 fi
 
+## Variables
+BLUE_APP_IOTA_PY_VIRT_ENV=`realpath .pyenv`
+export BOLOS_SDK=`realpath ../nanos-secure-sdk`
+export BOLOS_ENV=`realpath ../bolos-devenv`
+DIR_ARM_GCC=${BOLOS_ENV}/gcc-arm-none-eabi-5_3-2016q1/
+DIR_CLANG=${BOLOS_ENV}/clang-arm-fropi/
+
 print_ok "Updating Packages..."
 sudo apt update && sudo apt dist-upgrade
 
-print_ok "Installing LLVM, Clang and Python3 virtual environment..."
-sudo apt install clang-5.0 llvm5.0 libc6-i386 libc6-dev-i386 gcc-arm-none-eabi build-essential libudev-dev libusb-1.0-0-dev virtualenv npm python3-dev libudev-dev libusb-1.0-0-dev python3-pip python3-virtualenv python3-venv
+print_ok "Installing build-essential, libc6, libudev, libusb and Python3 virtual environment..."
+sudo apt install build-essential libc6-i386 libc6-dev-i386 libudev-dev libusb-1.0-0-dev virtualenv python3-dev python3-pip python3-virtualenv python3-venv
 
-export BLUE_APP_IOTA_PY_VIRT_ENV=`realpath .pyenv`
-export BOLOS_SDK=`realpath ../nanos-secure-sdk`
+if [ ! -d ${BOLOS_ENV} ]; then
+    mkdir -p ${BOLOS_ENV}
+fi
+
+if [ ! -d ${DIR_ARM_GCC} ]; then
+    print_ok "Downloading \"gcc-arm-none-eabi-5_3-2016q1\"..."
+    wget -N https://launchpad.net/gcc-arm-embedded/5.0/5-2016-q1-update/+download/gcc-arm-none-eabi-5_3-2016q1-20160330-linux.tar.bz2 -P ${BOLOS_ENV}/
+
+    print_ok "Unzipping \"gcc-arm-none-eabi-5_3-2016q1\"..."
+    tar xf ${BOLOS_ENV}/gcc-arm-none-eabi-5_3-2016q1-20160330-linux.tar.bz2 -C ${BOLOS_ENV}/
+fi
+
+if [ ! -d ${DIR_CLANG} ]; then
+    print_ok "Downloading \"clang+llvm-4.0.0\"..."
+    wget -N http://releases.llvm.org/4.0.0/clang+llvm-4.0.0-x86_64-linux-gnu-ubuntu-16.10.tar.xz -P ${BOLOS_ENV}/
+
+    print_ok "Unzipping \"clang+llvm-4.0.0\"..."
+    tar xf ${BOLOS_ENV}/clang+llvm-4.0.0-x86_64-linux-gnu-ubuntu-16.10.tar.xz -C ${BOLOS_ENV}/
+    mv ${BOLOS_ENV}/clang+llvm-4.0.0-x86_64-linux-gnu-ubuntu-16.10 ${BOLOS_ENV}/clang-arm-fropi
+fi
 
 if [ -d ${BOLOS_SDK} ]; then
-   print_ok "Update nanos-secure-sdk..."
+   print_ok "Updating nanos-secure-sdk..."
    cd ${BOLOS_SDK}
    git pull origin master
    git checkout tags/nanos-141
 else
-   print_ok "Download nanos-secure-sdk..."
+   print_ok "Downloading nanos-secure-sdk..."
    cd ..
    git clone https://github.com/LedgerHQ/nanos-secure-sdk.git
    git checkout tags/nanos-141
 fi
 
+## Adding environment variables t0 .bashrc
 if grep -q '^export BOLOS_SDK=' ~/.bashrc
 then
    # change BOLOS_SDK
-   print_ok "Update \"BOLOS_SDK\" environment variable in .bashrc..."
+   print_ok "Updating \"BOLOS_SDK\" environment variable in .bashrc..."
    sed -i -e "s;.*export BOLOS_SDK=.*;export BOLOS_SDK=\"${BOLOS_SDK}\";" ~/.bashrc
 else
    # add BOLOS_SDK
-   print_ok "Add \"BOLOS_SDK\" environment variable to .bashrc..."
+   print_ok "Adding \"BOLOS_SDK\" environment variable to .bashrc..."
    sed -i -e "$ a\export BOLOS_SDK=\"${BOLOS_SDK}\"" ~/.bashrc
 fi
 
-print_ok "Create python virtual environment..."
+if grep -q '^export BOLOS_ENV=' ~/.bashrc
+then
+   # change BOLOS_ENV
+   print_ok "Updating \"BOLOS_ENV\" environment variable in .bashrc..."
+   sed -i -e "s;.*export BOLOS_ENV=.*;export BOLOS_ENV=\"${BOLOS_ENV}\";" ~/.bashrc
+else
+   # add BOLOS_ENV
+   print_ok "Adding \"BOLOS_ENV\" environment variable to .bashrc..."
+   sed -i -e "$ a\export BOLOS_ENV=\"${BOLOS_ENV}\"" ~/.bashrc
+fi
+
+print_ok "Creating python virtual environment..."
 python3 -m venv ${BLUE_APP_IOTA_PY_VIRT_ENV}
 . ${BLUE_APP_IOTA_PY_VIRT_ENV}/bin/activate
 pip3 install setuptools --upgrade
@@ -70,7 +111,7 @@ print_ok "Adding user to the \"plugdev\" group..."
 sudo adduser $USER plugdev
 
 print_ok "Installing udev rules for ledger nano S..."
-# Additional Tags => TAG+="uaccess", TAG+="udev-acl" OWNER="<UNIX username>"
+# Additional Tags (if ledger is no recognized) => TAG+="uaccess", TAG+="udev-acl" OWNER="<UNIX username>"
 sudo bash -c "echo 'SUBSYSTEMS==\"usb\", ATTRS{idVendor}==\"2581\", ATTRS{idProduct}==\"1b7c\", MODE=\"0660\", GROUP=\"plugdev\"
 SUBSYSTEMS==\"usb\", ATTRS{idVendor}==\"2581\", ATTRS{idProduct}==\"2b7c\", MODE=\"0660\", GROUP=\"plugdev\"
 SUBSYSTEMS==\"usb\", ATTRS{idVendor}==\"2581\", ATTRS{idProduct}==\"3b7c\", MODE=\"0660\", GROUP=\"plugdev\"
