8e026c028a32207f16b112a00975c9e2ee0a50fe
Wolfgang Welz <welzwo@gmail.com>
2018-04-16 14:46:12 +0200

    Create nodejs demo, that actually signs a bundle

--- demos/nodejs/README.md
+++ demos/nodejs/README.md
@@ -1,3 +1,19 @@
 # NodeJS Demo
 
-Currently, there is no separate demo for trying IOTA using NodeJS and Ledger Nano S. Please go to the [special IOTA-branch on our ledgerjs-fork](https://github.com/IOTA-Ledger/ledgerjs/tree/iota-js) to run the tests there.
+### Install dependencies
+
+```bash
+yarn
+```
+
+### Build
+
+```bash
+yarn run build
+```
+
+### Run demo on the real device
+
+```bash
+yarn start
+```
---
+++ demos/nodejs/src/index.js
@@ -0,0 +1,58 @@
+import Transport from "@ledgerhq/hw-transport-node-hid";
+import IOTALedger from "hw-app-iota";
+import IOTA from "iota.lib.js";
+
+const BIP44_PATH = [
+    0x8000002C,
+    // use testnet path
+    0x80000001, // 0x8000107A,
+    0x80000000,
+    0x00000000,
+    0x00000000
+];
+const SECURITY_LEVEL = 2;
+
+const DEST_ADDRESS = "J9KPGBTWIKTRBIWXNDCZUWWWVVESYVISFJIY9GCMGVLQXFJBDAKLLN9PNAZOOUZFZDGDSFPWCTJYILDF9";
+const SRC_INDEX = 1;
+const VALUE = 10;
+const TAG = "TEST";
+const TIMESTAMP = Math.floor(Date.now() / 1000);
+
+
+(async () => {
+    const iota = new IOTA({
+        provider: 'https://nodes.testnet.iota.org:443/'
+    });
+
+    const transport = await Transport.create();
+    const ledger = new IOTALedger(transport);
+
+    // initialize
+    await ledger.setSeedInput(BIP44_PATH);
+    // get input address
+    const address = await ledger.getPubKey(SRC_INDEX);
+
+    console.log("create bundle; dest=%s, value=%i, src=%s", DEST_ADDRESS, VALUE, address);
+
+    var bundle = new iotaLib.utils.Bundle();
+
+    bundle.addEntry(1, DEST_ADDRESS, VALUE, TAG, TIMESTAMP, 0);
+    bundle.addEntry(2, address, -VALUE, TAG, TIMESTAMP, SRC_INDEX);
+    bundle.addTrytes([]);
+    bundle.finalize();
+
+    var inputMapping = {
+        address: SRC_INDEX
+    };
+    // sign the bundle on the ledger
+    bundle = await ledger.signBundle({
+        inputMapping,
+        bundle,
+        security: SECURITY_LEVEL
+    });
+
+    // dump signed bundle
+    console.log(bundle);
+})().catch(e => {
+    console.error(e);
+});
