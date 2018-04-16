9e48e82a18c7cf77faa48233cd93ce69eb4f51d1
Wolfgang Welz <welzwo@gmail.com>
2018-04-16 13:13:55 +0200

    Add U2F demo

---
+++ demos/u2f/src/index.js
@@ -0,0 +1,51 @@
+import "babel-polyfill";
+
+import Transport from "@ledgerhq/hw-transport-u2f";
+import AppBtc from "@ledgerhq/hw-app-btc";
+import AppIota from "hw-app-iota";
+
+// use testnet path
+const BIP44_PATH = [
+    0x8000002C,
+    0x80000001,
+    0x80000000,
+    0x00000000,
+    0x00000000
+];
+const SECURITY_LEVEL = 2;
+
+const errorEl = document.createElement("code");
+errorEl.style.color = "#a33";
+const pre = document.createElement("pre");
+pre.appendChild(errorEl);
+document.body.appendChild(pre);
+
+
+const getBtcAddress = async () => {
+    const transport = await Transport.create();
+    const btc = new AppBtc(transport);
+
+    const result = await btc.getWalletPublicKey("44'/0'/0'/0");
+    return result.bitcoinAddress;
+};
+
+const getIotaAddress = async index => {
+    try {
+        const transport = await Transport.create();
+        transport.setDebugMode(true);
+        // transport.setScrambleKey("IOTA");
+        const iota = new AppIota(transport);
+
+        await iota.setSeedInput(BIP44_PATH, SECURITY_LEVEL);
+        return await ledger.getPubKey(0);
+    } catch (e) {
+        console.error(e);
+        errorEl.textContent = e.message;
+    }
+};
+
+// getBtcAddress().then(a => console.log(a));
+getIotaAddress().then(a => {
+    console.log(a);
+    document.write(a);
+});
