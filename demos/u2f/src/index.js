9d68800799d15d65ec9b738f0e9d1a78ce1800b9
Wolfgang Welz <welzwo@gmail.com>
2018-04-17 12:13:19 +0200

    Catch error

--- demos/u2f/src/index.js
+++ demos/u2f/src/index.js
@@ -14,38 +14,39 @@ const BIP44_PATH = [
 ];
 const SECURITY_LEVEL = 2;
 
+const getBtcAddress = async () => {
+    const transport = await Transport.create();
+    const btc = new AppBtc(transport);
+    const result = await btc.getWalletPublicKey("44'/0'/0'/0");
+    return result.bitcoinAddress;
+};
+
+const getIotaAddress = async () => {
+
+    const transport = await Transport.create();
+    transport.setDebugMode(true);
+    // transport.setScrambleKey("IOTA");
+    const iota = new AppIota(transport);
+
+    await iota.setSeedInput(BIP44_PATH, SECURITY_LEVEL);
+    return await ledger.getPubKey(0);
+};
+
+
 const errorEl = document.createElement("code");
 errorEl.style.color = "#a33";
 const pre = document.createElement("pre");
 pre.appendChild(errorEl);
 document.body.appendChild(pre);
 
-
-const getBtcAddress = async () => {
-    const transport = await Transport.create();
-    const btc = new AppBtc(transport);
-
-    const result = await btc.getWalletPublicKey("44'/0'/0'/0");
-    return result.bitcoinAddress;
-};
-
-const getIotaAddress = async index => {
-    try {
-        const transport = await Transport.create();
-        transport.setDebugMode(true);
-        // transport.setScrambleKey("IOTA");
-        const iota = new AppIota(transport);
-
-        await iota.setSeedInput(BIP44_PATH, SECURITY_LEVEL);
-        return await ledger.getPubKey(0);
-    } catch (e) {
+// getBtcAddress().then(a => console.log(a));
+getIotaAddress().then(
+    a => {
+        console.log(a);
+        document.write(a);
+    },
+    e => {
         console.error(e);
         errorEl.textContent = e.message;
     }
-};
-
-// getBtcAddress().then(a => console.log(a));
-getIotaAddress().then(a => {
-    console.log(a);
-    document.write(a);
-});
+);
