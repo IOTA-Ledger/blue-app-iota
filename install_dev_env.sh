a7345354ccd61efe9ebc52051f7b39ea1cdfc0f5
muXxer <mux3r@web.de>
2018-05-26 15:05:21 +0200

    Add several linux distributions to the version check

--- install_dev_env.sh
+++ install_dev_env.sh
@@ -18,21 +18,28 @@ function print_warn { printf "${_bold}${_yellow}%s${_reset}\n" "$@"; }
 print_ok "Installing development environment for blue-app-iota..."
 
 if ! type "gawk" &> /dev/null; then
-  print_err "ERROR: gawk is missing! If you're on Ubuntu, please run 'sudo apt install -y gawk'"
+  print_err "ERROR: gawk is missing! If you're on Debian or Ubuntu, please run 'sudo apt install -y gawk'"
   exit 1
 fi
 
-linux_distr=`gawk -F= '/^NAME/{print $2}' /etc/os-release`
-linux_codename=`gawk -F= '/^VERSION_CODENAME/{print $2}' /etc/os-release`
-if [ ${linux_distr} != '"Ubuntu"' ];
+linux_id=`gawk -F= '/^ID=/{print $2}' /etc/os-release`
+linux_version_id=`gawk -F= '/^VERSION_ID=/{print $2}' /etc/os-release`
+if [ ${linux_id} = 'debian' ];
 then
-    print_err "ERROR: This script was created for Ubuntu only! Exiting..."
-    exit 1
-fi
-
-if [ ${linux_codename} != 'artful' ];
+    if [ ${linux_version_id} != '"9"' ];
+    then
+        print_err "ERROR: This script was tested under Debian 9 (stretch) only! Exiting..."
+        exit 1
+    fi
+elif [ ${linux_id} = 'ubuntu' ];
 then
-    print_err "ERROR: This script was tested under Ubuntu Artful Aardvark only! Exiting..."
+    if ! [[ "${linux_version_id}" =~ ^('"16.04"'|'"16.10"'|'"17.04"'|'"17.10"'|'"18.04"')$ ]];
+    then
+        print_err "ERROR: This script was tested under Ubuntu 16.04 till 18.04 only! Exiting..."
+        exit 1
+	fi
+else
+    print_err "ERROR: This script was created for Debian and Ubuntu only! Exiting..."
     exit 1
 fi
 
@@ -46,8 +53,8 @@ DIR_CLANG=${BOLOS_ENV}/clang-arm-fropi/
 print_ok "Updating Packages..."
 sudo apt update && sudo apt dist-upgrade -y
 
-print_ok "Installing build-essential, libc6, libudev, libusb and Python3 virtual environment..."
-sudo apt install -y build-essential libc6-i386 libc6-dev-i386 libudev-dev libusb-1.0-0-dev virtualenv python3-dev python3-pip python3-virtualenv python3-venv
+print_ok "Installing development environment packages..."
+sudo apt install -y tar wget sed grep pkg-config git autoconf autogen libtool build-essential libc6-i386 libc6-dev-i386 libudev-dev libusb-1.0-0-dev python3-dev python3-pip python3-venv
 
 if [ ! -d ${BOLOS_ENV} ]; then
     mkdir -p ${BOLOS_ENV}
@@ -109,7 +116,8 @@ fi
 print_ok "Creating python virtual environment..."
 python3 -m venv ${BLUE_APP_IOTA_PY_VIRT_ENV}
 . ${BLUE_APP_IOTA_PY_VIRT_ENV}/bin/activate
-pip3 install setuptools --upgrade
+pip3 install --upgrade pip
+pip3 install --upgrade setuptools
 pip3 install wheel
 #pip3 install ledgerblue    # This doesn't work at the moment => Better use the git repo!
 SECP_BUNDLED_EXPERIMENTAL=1 pip3 --no-cache-dir install --no-binary secp256k1 secp256k1
@@ -128,8 +136,8 @@ SUBSYSTEMS==\"usb\", ATTRS{idVendor}==\"2581\", ATTRS{idProduct}==\"1807\", MODE
 SUBSYSTEMS==\"usb\", ATTRS{idVendor}==\"2581\", ATTRS{idProduct}==\"1808\", MODE=\"0660\", GROUP=\"plugdev\"
 SUBSYSTEMS==\"usb\", ATTRS{idVendor}==\"2c97\", ATTRS{idProduct}==\"0000\", MODE=\"0660\", GROUP=\"plugdev\"
 SUBSYSTEMS==\"usb\", ATTRS{idVendor}==\"2c97\", ATTRS{idProduct}==\"0001\", MODE=\"0660\", GROUP=\"plugdev\"' >/etc/udev/rules.d/20-hw1.rules"
-sudo udevadm trigger
 sudo udevadm control --reload-rules
+sudo udevadm trigger
 
 print_ok "...Installing development environment for blue-app-iota done!"
 print_warn "Please restart to apply changes!"
