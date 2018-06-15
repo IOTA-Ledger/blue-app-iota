3be3e28f99e6a86026c0db486ee84461221afbd3
Wolfgang Welz <welzwo@gmail.com>
2018-06-16 00:52:42 +0200

    Test addresses with checksum

--- tests/address_test.c
+++ tests/address_test.c
@@ -8,29 +8,27 @@
 static void seed_address(const char *seed_chars, uint32_t idx, uint8_t security,
                          char *address_chars)
 {
-    if (security > 3 || strlen(seed_chars) != NUM_HASH_TRYTES) {
-        address_chars = "";
-        return;
-    }
-
     unsigned char seed_bytes[NUM_HASH_BYTES];
     chars_to_bytes(seed_chars, seed_bytes, NUM_HASH_TRYTES);
 
     unsigned char address_bytes[NUM_HASH_BYTES];
     get_public_addr(seed_bytes, idx, security, address_bytes);
 
-    bytes_to_chars(address_bytes, address_chars, NUM_HASH_BYTES);
-    // make null-terminated
-    address_chars[NUM_HASH_TRYTES] = '\0';
+    get_address_with_checksum(address_bytes, address_chars);
 }
 
 static void test_address(const char *seed, uint32_t idx, uint8_t security,
-                         const char *expected)
+                         bool has_checksum, const char *expected)
 {
-    char output[MAX_NUM_TRYTES + 1];
+    char output[NUM_HASH_TRYTES + NUM_CHECKSUM_TRYTES];
     seed_address(seed, idx, security, output);
 
-    assert_memory_equal(output, expected, NUM_HASH_TRYTES);
+    int address_length = NUM_HASH_TRYTES;
+    if (has_checksum) {
+        address_length += NUM_CHECKSUM_TRYTES;
+    }
+
+    assert_memory_equal(output, expected, address_length);
 }
 
 static void test_vector(void **state, const TEST_VECTOR *vector,
@@ -39,7 +37,8 @@ static void test_vector(void **state, const TEST_VECTOR *vector,
     uint32_t idx = (uintptr_t)*state;
     assert_in_range(idx, 0, MAX_ADDRESS_INDEX);
 
-    test_address(vector->seed, idx, security, vector->addresses[security][idx]);
+    test_address(vector->seed, idx, security, true,
+                 vector->addresses[security][idx]);
 }
 
 static void test_security_level_one(void **state)
@@ -79,7 +78,7 @@ static void test_n_addresses_for_seed(void **state)
     void test(char *hashes[])
     {
         for (uint32_t idx = 0; idx < 4; idx++) {
-            test_address(hashes[0], idx, 2, hashes[idx + 1]);
+            test_address(hashes[0], idx, 2, false, hashes[idx + 1]);
         }
     }
 
