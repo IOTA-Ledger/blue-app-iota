49afb3103d1d005b98a44ba02ea578f297faed21
Wolfgang Welz <welzwo@gmail.com>
2018-06-24 18:57:49 +0200

    Add comments for address generation

--- src/iota/addresses.h
+++ src/iota/addresses.h
@@ -3,12 +3,20 @@
 
 #include "iota_types.h"
 
+/** @brief Computes the public address.
+ *  @parm seed_bytes the seed as big-endian 48-byte integer
+ *  @idx index of the address
+ *  @security securtiy level
+ *  @address_bytes address as a big-endian 48-byte integer
+ */
 void get_public_addr(const unsigned char *seed_bytes, uint32_t idx,
                      unsigned int security, unsigned char *address_bytes);
 
 /** @brief Computes the full address string in base-27 encoding.
  *  The full address consists of the actual address (81 chars) plus 9 chars of
  *  checksum.
+ *  @param address_bytes the address, i.e. hash, in binarary representation
+ *  @full_address target for the full address as a string
  */
 void get_address_with_checksum(const unsigned char *address_bytes,
                                char *full_address);
