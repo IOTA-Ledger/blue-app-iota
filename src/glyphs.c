29755ddcbf0c9323943efa7cdc3e9792746ed518
Tyler <5952757+TylerHann@users.noreply.github.com>
2018-09-28 22:25:38 -0600

    added custom back and iota glyphs

---
+++ src/glyphs.c
@@ -0,0 +1,30 @@
+#include "glyphs.h"
+unsigned int const C_icon_back_colors[]
+ = {
+  0x00000000, 
+  0x00ffffff, 
+};
+	
+unsigned char const C_icon_back_bitmap[] = {
+0xe0, 0x01, 0xfe, 0xc1, 0xfd, 0x38, 0x7f, 0x06, 0xdf, 0x81, 0xff, 0xc4, 0x7f, 0xf3, 0xff, 0xbc, 
+  0x1f, 0xe7, 0xe7, 0xf1, 0x3f, 0xf8, 0x07, 0x78, 0x00, };
+
+#ifdef OS_IO_SEPROXYHAL
+#include "os_io_seproxyhal.h"
+const bagl_icon_details_t C_icon_back = { GLYPH_icon_back_WIDTH, GLYPH_icon_back_HEIGHT, 1, C_icon_back_colors, C_icon_back_bitmap };
+#endif // OS_IO_SEPROXYHAL
+#include "glyphs.h"
+unsigned int const C_icon_iota_colors[]
+ = {
+  0x00000000, 
+  0x00ffffff, 
+};
+	
+unsigned char const C_icon_iota_bitmap[] = {
+0xe0, 0x01, 0xfe, 0xc1, 0x9f, 0xf8, 0x67, 0xd6, 0xdf, 0x7f, 0x9f, 0xff, 0xe7, 0xfb, 0xdf, 0xbf, 
+  0xff, 0xe7, 0xcb, 0xf1, 0x33, 0xf8, 0x07, 0x78, 0x00, };
+
+#ifdef OS_IO_SEPROXYHAL
+#include "os_io_seproxyhal.h"
+const bagl_icon_details_t C_icon_iota = { GLYPH_icon_iota_WIDTH, GLYPH_icon_iota_HEIGHT, 1, C_icon_iota_colors, C_icon_iota_bitmap };
+#endif // OS_IO_SEPROXYHAL
