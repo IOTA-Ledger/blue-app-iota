e9102ec0f5b8445e1b1277f115b460a2ca01cd12
Wolfgang Welz <welzwo@gmail.com>
2018-11-15 18:01:11 +0100

    Fix reset tests

--- tests/reset_test.c
+++ tests/reset_test.c
@@ -7,8 +7,8 @@
 void seed_derive_from_bip32(const unsigned int *path, unsigned int pathLength,
                             unsigned char *seed_bytes)
 {
-    UNUSED(path);
-    UNUSED(pathLength);
+    check_expected(path);
+    check_expected(pathLength);
 
     chars_to_bytes(mock_ptr_type(char *), seed_bytes, NUM_HASH_TRYTES);
 }
@@ -20,13 +20,6 @@ void io_send(const void *ptr, unsigned int length, unsigned short sw)
     check_expected(sw);
 }
 
-static void assert_chars_equals_bytes(const char *a, const unsigned char *b)
-{
-    unsigned char bytes[NUM_HASH_BYTES] = {0};
-    chars_to_bytes(a, bytes, NUM_HASH_TRYTES);
-    assert_memory_equal(b, bytes, NUM_HASH_BYTES);
-}
-
 static void test_reset_initialized(void **state)
 {
     UNUSED(state);
@@ -34,67 +27,40 @@ static void test_reset_initialized(void **state)
     api_initialize();
 
     unsigned char input[0]; // no input
-    EXPECT_API_OK(reset, P1_RESET_EVERYTHING, input);
+    EXPECT_API_OK(reset, 0, input);
 
     assert_all_zero(&api, sizeof(api));
 }
 
-static void test_reset_seed(void **state)
+static void test_reset_pubkey(void **state)
 {
     UNUSED(state);
     static const int security = 2;
-    static const int idx = 0;
 
     api_initialize();
-    EXPECT_API_SET_SEED_OK(PETER_VECTOR.seed, security);
+    {
+        SET_SEED_PUBKEY_INPUT input;
+        SET_SEED_IN_INPUT(PETER_VECTOR.seed, security, &input);
+        input.pubkey.address_idx = 0;
+
+        PUBKEY_OUTPUT output;
+        strncpy(output.address, PETER_VECTOR.addresses[security][0],
+                NUM_HASH_TRYTES);
+
+        EXPECT_API_DATA_OK(pubkey, 0, input, output);
+    }
     {
         unsigned char input[0]; // no input
-        EXPECT_API_OK(reset, P1_RESET_EVERYTHING, input);
+        EXPECT_API_OK(reset, 0, input);
     }
 
-    // internally everything should be zero
     assert_all_zero(&api, sizeof(api));
 
     {
-        PUBKEY_INPUT input = {idx};
-        EXPECT_API_EXCEPTION(pubkey, 0, input);
-    }
-}
+        SET_SEED_PUBKEY_INPUT input;
+        SET_SEED_IN_INPUT(PETER_VECTOR.seed, security, &input);
+        input.pubkey.address_idx = 0;
 
-static void test_reset_partial_path(void **state)
-{
-    UNUSED(state);
-    static const int security = 2;
-
-    api_initialize();
-    EXPECT_API_SET_SEED_OK(PETER_VECTOR.seed, security);
-    EXPECT_API_SET_BUNDLE_OK(PETER_VECTOR.bundle, 2, PETER_VECTOR.bundle_hash);
-    {
-        unsigned char input[0]; // no input
-        EXPECT_API_OK(reset, P1_RESET_PARTIAL, input);
-    }
-
-    // internally the path should not have changed
-    static const unsigned int path[BIP32_PATH_LENGTH] = BIP32_PATH;
-    assert_memory_equal(api.bip32_path, path, sizeof(path));
-}
-
-static void test_reset_partial_pubkey(void **state)
-{
-    UNUSED(state);
-    static const int security = 2;
-
-    api_initialize();
-    EXPECT_API_SET_SEED_OK(PETER_VECTOR.seed, security);
-    EXPECT_API_SET_BUNDLE_OK(PETER_VECTOR.bundle, 2, PETER_VECTOR.bundle_hash);
-    {
-        unsigned char input[0]; // no input
-        EXPECT_API_OK(reset, P1_RESET_PARTIAL, input);
-    }
-
-    // same address should be returned
-    {
-        PUBKEY_INPUT input = {0};
         PUBKEY_OUTPUT output;
         strncpy(output.address, PETER_VECTOR.addresses[security][0],
                 NUM_HASH_TRYTES);
@@ -103,56 +69,38 @@ static void test_reset_partial_pubkey(void **state)
     }
 }
 
-static void test_reset_partial_bundle(void **state)
+static void test_reset_bundle(void **state)
 {
     UNUSED(state);
     static const int security = 2;
 
     api_initialize();
-    EXPECT_API_SET_SEED_OK(PETER_VECTOR.seed, security);
-    EXPECT_API_SET_BUNDLE_OK(PETER_VECTOR.bundle, 2, PETER_VECTOR.bundle_hash);
+    { // set first tx of bundle
+        SET_SEED_TX_INPUT input;
+        SET_SEED_IN_INPUT(PETER_VECTOR.seed, security, &input);
+        memcpy(&input.tx, &PETER_VECTOR.bundle[0], sizeof(TX_INPUT));
+
+        TX_OUTPUT output = {0};
+        output.finalized = false;
+
+        EXPECT_API_DATA_OK(tx, P1_FIRST, input, output);
+    }
     {
         unsigned char input[0]; // no input
-        EXPECT_API_OK(reset, P1_RESET_PARTIAL, input);
+        EXPECT_API_OK(reset, 0, input);
     }
 
-    // internally the seed should not have changed
-    assert_chars_equals_bytes(PETER_VECTOR.seed, api.seed_bytes);
+    assert_all_zero(&api, sizeof(api));
 
-    // we can set a new bundle
-    EXPECT_API_SET_BUNDLE_OK(PETER_VECTOR.bundle, 2, PETER_VECTOR.bundle_hash);
-}
-
-static void test_reset_partial_no_seed(void **state)
-{
-    UNUSED(state);
-
-    api_initialize();
-
-    unsigned char input[0]; // no input
-    EXPECT_API_EXCEPTION(reset, P1_RESET_PARTIAL, input);
-}
-
-static void test_invalid_p1(void **state)
-{
-    UNUSED(state);
-
-    api_initialize();
-
-    unsigned char input[0]; // no input
-    EXPECT_API_EXCEPTION(reset, 0xFF, input);
+    EXPECT_API_SET_BUNDLE_OK(PETER_VECTOR.seed, security, PETER_VECTOR.bundle,
+                             2, PETER_VECTOR.bundle_hash);
 }
 
 int main(void)
 {
-    const struct CMUnitTest tests[] = {
-        cmocka_unit_test(test_reset_initialized),
-        cmocka_unit_test(test_reset_seed),
-        cmocka_unit_test(test_reset_partial_path),
-        cmocka_unit_test(test_reset_partial_pubkey),
-        cmocka_unit_test(test_reset_partial_bundle),
-        cmocka_unit_test(test_reset_partial_no_seed),
-        cmocka_unit_test(test_invalid_p1)};
+    const struct CMUnitTest tests[] = {cmocka_unit_test(test_reset_initialized),
+                                       cmocka_unit_test(test_reset_pubkey),
+                                       cmocka_unit_test(test_reset_bundle)};
 
     return cmocka_run_group_tests(tests, NULL, NULL);
 }
